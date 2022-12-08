ee313df6be2aafad1045722ae0bcaac61bafcd92
Michael Lapierre-Ouellette <112026857+mlouellette@users.noreply.github.com>
2022-12-07 22:20:07 -0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,106 @@
+function myFunction() {
+    
+
+  }
+
+function comingSoon() {
+    alert("COMING SOON !");
+
+  }
+  
+
+// Hamburger menu ------------------
+  
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});  
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
+
+// Form Validation -----------------------
+
+const form = document.getElementById('myForm');
+const username = document.getElementById('inputName');
+const email = document.getElementById('inputEmail');
+const phone = document.getElementById('inputPhone');
+const dropdown = document.getElementById("inputDropdown");
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+
+};
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const phoneValue = phone.value.trim();
+  const dropDownValue = dropdown.value.trim();
+  
+
+  if(usernameValue === '') {
+      setError(username, 'Username is required');
+  } else {
+      setSuccess(username);
+  }
+
+  if(emailValue === '') {
+      setError(email, 'Email is required');
+  } else if (!isValidEmail(emailValue)) {
+      setError(email, 'Provide a valid email address');
+  } else {
+      setSuccess(email);
+  }
+
+  if(phoneValue === '') {
+      setError(phone, 'Phone number is required');
+  } else if (phoneValue.length < 9 ) {
+      setError(phone, 'Phone field must be at least 9 number.')
+  } else {
+      setSuccess(phone);
+  }
+
+  if(dropDownValue === 'Select') {
+    setError(dropdown, 'Language is required');
+} else {
+    setSuccess(dropdown);
+}
+
+
+
+  
+
+};
+
+
+
